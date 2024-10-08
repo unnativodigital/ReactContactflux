7059cb0a8ec4f838e91d7e2d8de486630122d189
Francisco Capone <francisco.capone@gmail.com>
2024-10-07 21:34:47 -0300

    Mejoras en diseño

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -24,56 +24,28 @@ const ContactCard = ({ contact }) => {
 
   }
   return (
-    <li className="list-group-item shadow p-3 mb-5 rounded rounded-3 " style={{ backgroundColor: "#3c4d67" }}>
-      <div className=" row p-3 ">
-        <div className="contact-img-wrapper col-3 ">
-          <img src="https://cdn-icons-png.flaticon.com/512/10337/10337609.png"
-            alt="image profile person"
-            className="contact-img ms-5"
-          />
-        </div>
-
-        <ul className="list-unstyled col-7">
-          <li className="py-2">
-            <p className="fs-4 fw-bold text-light">
-              {contact.name}
-            </p>
-          </li>
-          <li className="fw-bolder text-light ">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-geo-alt-fill " viewBox="0 0 16 16">
-              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
-            </svg>
-            <span className="text-light ms-2">  {contact.address}</span>
-          </li>
-          <li className=" text-light fw-light">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-telephone-fill me-3 " viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-            </svg>
-            {contact.phone}
-          </li>
-          <li className="text-light">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-envelope-fill me-3 " viewBox="0 0 16 16">
-              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
-            </svg>
-            <small>{contact.email}</small>
-          </li>
-        </ul>
-
-        <div className="contact-accions col-2">
-          <Link to={`/update/${contact.id}`} className="btn btn-custom-edit me-3" >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"></path>
-            </svg>
-          </Link>
-          <button type="button" class="btn btn-custom-del" onClick={() => { handleDelete(contact.id) }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"></path>
-            </svg>
-          </button>
+    <div className="col-lg-6 col-xl-4 col-md-6 col-sm-12">
+      <div className="card card-default p-2 contact-card">
+        <img src="https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png" className="mr-3 img-fluid rounded" alt="Avatar Image" />
+        <div className="media-body ms-3">
+          <h5 className="mt-0 mb-2 text-dark">{contact.name}</h5>
+          <ul className="list-unstyled text-smoke text-smoke">
+            <li className="d-flex">
+              <i className="mdi mdi-map mr-1"></i>
+              <span>{contact.address}</span>
+            </li>
+            <li className="d-flex">
+              <i className="mdi mdi-email mr-1"></i>
+              <span>{contact.email}</span>
+            </li>
+            <li className="d-flex">
+              <i className="mdi mdi-phone mr-1"></i>
+              <span>{contact.phone}</span>
+            </li>
+          </ul>
         </div>
       </div>
-    </li >
+    </div>
   );
 };
 
--- src/views/ContactList.jsx
+++ src/views/ContactList.jsx
@@ -8,30 +8,37 @@ const ContactList = () => {
   const { store } = useContext(Context);
   // console.log(store.todos);
   return (
-    <div className="container ">
-      <div className="d-flex justify-content-end py-4">
-        <Link to="/addcontact">
-          <button className='btn btn-success'>Add new contact</button>
-        </Link>
-      </div>
-      {
-        !store.contactList || !store.contactList.length ?
-        <div>No tienes contactos agregados</div>
-        :
-      <ul className="list-group col-12">
-        {
-          store.contactList.map((contact) => {
-            return (
-              <ContactCard
-                key={contact.id}
-                contact={contact}
-              />
-            )
-          })
-        }
-      </ul>
-      }
-    </div>
+          <div className="container">
+            <div className="content contact-list">
+              <div className="card card-default">
+                <div className="card-header align-items-center px-3 px-md-5">
+                  <h2>Contacts</h2>
+                  <Link to="/addcontact">
+                    <button className='btn btn-primary'>Add new contact</button>
+                  </Link>
+                </div>
+                <div className="card-body px-3 px-md-5">
+                {
+                  !store.contactList || !store.contactList.length ?
+                  <div>No tienes contactos agregados</div>
+                  :
+                  <div className="row">
+                    {
+                      store.contactList.map((contact) => {
+                        return (
+                          <ContactCard
+                            key={contact.id}
+                            contact={contact}
+                          />
+                        )
+                      })
+                    }
+                  </div>
+                }
+                </div>
+              </div>
+            </div>
+          </div>
   );
 };
 
